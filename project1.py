a9e651a3ad53ec5bc956f0b86d223e41a578bfae
tomczyk <michal12tomczyk@gmail.com>
2023-03-28 22:45:50 +0200

    better GUI

--- project1.py
+++ project1.py
@@ -2,7 +2,7 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
+from tkinter import Frame
 import wave
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
@@ -11,84 +11,76 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 
 class GUI:
 
+    def __init__(self):
+        self.times = None
+        self.audio_normalised = None
+        self.volume = None
+        self.window = tk.Tk()
+        self.window.title("Sound analysing app")
+        self.window.geometry("900x600+50+50")
+        ttk.Label(self.window, text="Upload a .wav file").grid(row=0, column=0)
+        button = ttk.Button(self.window, text="Open", command=self.open_file)
+        button.grid(row=1, column=0, padx=5, pady=5)
+        self.plot_basic()
+        self.plot_volume()
+        self.window.mainloop()
+
     def analyze(self, file):
         frames = file.getnframes()
         audio = file.readframes(frames)
         rate = file.getframerate()
         duration = frames / rate
-        times = np.linspace(0, frames / rate, num=frames)
+        self.times = np.linspace(0, frames / rate, num=frames)
         audio_as_np_int16 = np.frombuffer(audio, dtype=np.int16)
         audio_as_np_float32 = audio_as_np_int16.astype(np.float32)
         # Normalise float32 array so that values are between -1.0 and +1.0
         max_int16 = 2 ** 15
-        audio_normalised = audio_as_np_float32 / max_int16
-        ttk.Label(self.window, text="The file is " + str(duration) + " seconds long").pack()
+        self.audio_normalised = audio_as_np_float32 / max_int16
+        ttk.Label(self.window, text="Length: " + str(duration) + "s").grid(row=2, column=0, pady=5, padx=2)
         channel_num = file.getnchannels()
-        string_3 = " channel"
         if channel_num > 1:
-            string_3 += "s"
-            ttk.Label(self.window, text="So far focusing only on first channel").pack()
-            audio_normalised = audio_normalised[1::channel_num]
-        ttk.Label(self.window, text="The sound has " + str(channel_num) + string_3).pack()
-        self.plot_basic(audio_normalised, times)
-        volume = [0 for i in range(0, frames - 4, 256)]
+            ttk.Label(self.window, text="Choosing channel 1").grid(row=4, column=0, pady=5, padx=2)
+            self.audio_normalised = self.audio_normalised[1::channel_num]
+        ttk.Label(self.window, text="No. of channels: " + str(channel_num)).grid(row=3, column=0, pady=5, padx=2)
+        self.volume = [0 for i in range(0, frames - 4, 256)]
         j = 0
         for i in range(0, frames - 255, 256):
-            volume[j] = np.sqrt(0.2 * sum(audio_normalised[i:i + 255] ** 2))
+            self.volume[j] = np.sqrt(0.2 * sum(self.audio_normalised[i:i + 255] ** 2))
             j += 1
-        self.plot_volume(volume)
+        self.plot_basic()
+        self.plot_volume()
 
-        # self.plot_spectrogram(audio, rate)
-
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Sound analysing app")
-        self.window.geometry("900x600+50+50")
-        ttk.Label(self.window, text="Upload a .wav file").pack()
-        ttk.Button(self.window, text="Open", command=self.open_file).pack()
-        self.window.mainloop()
-
-    def plot_spectrogram(self, audio, freq):
-        # to be fixed
-        fig = Figure()
+    def plot_basic(self):
+        fig = Figure(figsize=(8, 3), dpi=100)
         plot1 = fig.add_subplot(111)
-        plot1.specgram(audio, Fs=freq, vmin=-20, vmax=50)
-        plot1.set_xlabel("Time (s)")
-        plot1.set_ylabel("Frequency (Hz)")
-        plot1.set_title("Spectrogram")
-        plot1.colorbar()
-        canvas = FigureCanvasTkAgg(fig, master=self.window)
-        canvas.draw()
-        canvas.get_tk_widget().pack()
-        toolbar = NavigationToolbar2Tk(canvas, self.window)
-        toolbar.update()
-        canvas.get_tk_widget().pack()
-
-    def plot_basic(self, audio, times):
-        fig = Figure()
-        plot1 = fig.add_subplot(111)
-        plot1.plot(times, audio)
+        if self.times is not None and self.audio_normalised is not None:
+            plot1.plot(self.times, self.audio_normalised)
         plot1.set_ylabel("Amplitude")
         plot1.set_xlabel("Time (s)")
         canvas = FigureCanvasTkAgg(fig, master=self.window)
         canvas.draw()
-        canvas.get_tk_widget().pack()
-        toolbar = NavigationToolbar2Tk(canvas, self.window)
+        canvas.get_tk_widget().grid(row=1, column=1, rowspan=8, columnspan=3, padx=10, pady=10)
+        toolbar_frame = Frame(master=self.window)
+        toolbar_frame.grid(row=9, column=1)
+        toolbar = NavigationToolbar2Tk(canvas, toolbar_frame)
         toolbar.update()
-        canvas.get_tk_widget().pack()
+        canvas.get_tk_widget().grid(row=1, column=1)
 
-    def plot_volume(self, volume):
-        fig = Figure()
+    def plot_volume(self):
+        fig = Figure(figsize=(8, 3), dpi=100)
         plot1 = fig.add_subplot(111)
-        plot1.plot(volume)
+        if self.volume is not None:
+            plot1.plot(self.volume)
         plot1.set_ylabel("volume")
         plot1.set_xlabel("time")
         canvas = FigureCanvasTkAgg(fig, master=self.window)
         canvas.draw()
-        canvas.get_tk_widget().pack()
-        toolbar = NavigationToolbar2Tk(canvas, self.window)
+        canvas.get_tk_widget().grid(row=10, column=1, rowspan=8, columnspan=3, pady=10, padx=10)
+        toolbar_frame = Frame(master=self.window)
+        toolbar_frame.grid(row=18, column=1)
+        toolbar = NavigationToolbar2Tk(canvas, toolbar_frame)
         toolbar.update()
-        canvas.get_tk_widget().pack()
+        canvas.get_tk_widget().grid(row=10, column=1)
 
     def open_file(self):
         filename = fd.askopenfilename(
